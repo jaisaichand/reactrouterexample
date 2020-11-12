ef216a5e720bc56c6bdb04d61d496b04be16a20b
JAI <t.jaisaichand4@gmail.com>
2020-11-12 21:03:13 +0530

    component interaction

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import Shop from './Shop';
 
-function Home() {
-    return (
-        <div className="App">
-            Home works
-        </div>
-    );
+class Home extends React.Component {
+    state = {
+        counter: 0,
+        email: 'jaisaichand4',
+        mobile: 9100341041
+    }
+    incrementCnt = () => {
+        this.setState({ counter: this.state.counter + 1 })
+    }
+
+    setNewDetails = (dat) => {
+        console.log(dat);
+        this.setState({ email: dat.email, mobile: dat.mobile })
+    }
+    render() {
+        return (
+            <div className="App">
+                <p>{this.state.counter}</p>
+                <button onClick={this.incrementCnt}>Add</button>
+                <div>
+                    <p>{this.state.email}</p>
+                    <p>{this.state.mobile}</p>
+                </div>
+                <Shop email={this.state.email} mobile={this.state.mobile} getupdateddata={this.setNewDetails} />
+            </div>
+        );
+    }
 }
 
 export default Home;
